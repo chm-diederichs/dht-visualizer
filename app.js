da689aea790a4fa44c7f92e092194641b6787c2e
Christophe Diederichs <chris.d.itunes@gmail.com>
2020-12-22 17:40:04 +0100

    use process.env.PORT by default

--- app.js
+++ app.js
@@ -4,19 +4,19 @@ var path = require('path');
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.get('/index.js', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.js'));
+  res.sendFile(path.join(__dirname + '/index.js'));
 });
 
 app.get('/locations.json', function(req, res) {
-    res.sendFile(path.join(__dirname + '/locations.json'));
+  res.sendFile(path.join(__dirname + '/locations.json'));
 });
 
 app.get('/marker.webp', function(req, res) {
-    res.sendFile(path.join(__dirname + '/marker.webp'));
+  res.sendFile(path.join(__dirname + '/marker.webp'));
 });
 
-app.listen(process.argv[2]);
+app.listen(process.env.PORT || process.argv[2] || 10001);
